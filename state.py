6bea4823e2540fa944b42a89b36d663a666360c5
sondrehd <sondredyvik@gmail.com>
2015-09-17 15:48:13 +0200

    minor changes to state and astar

--- state.py
+++ state.py
@@ -1,11 +1,18 @@
 __author__ = 'sondredyvik'
 class State():
-    def __init__(xpos,ypos,board):
-       return
+    def __init__(self,xpos,ypos,board):
+        self.dimensions = board.dimensions
+        self.xpos = xpos
+        self.ypos = ypos
+        self.parent = None
+        self.g = float('inf')
+        return
+
     def __repr__(self):
         return self.type
     def reparent(self, parent):
         self.parent = parent
+        self.g = self.parent.g +1
     def calculateHeuristic(self,goalnode):
         self.h = abs(self.xpos - goalnode.xpos)+ abs (self.ypos - goalnode.ypos)
     def calculateNeighbours(self):
