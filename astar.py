df1e0124f850103121765c2d4070dcd92c6667fc
sondrehd <sondredyvik@gmail.com>
2015-09-18 18:03:13 +0200

    trololol

--- astar.py
+++ astar.py
@@ -11,6 +11,7 @@ class Astar:
         self.type ="astar"
 
         self.openlist =[]
+        self.closedlist=[]
         self.openset = set()
         self.closedset = set()
         #first node is created
@@ -18,33 +19,47 @@ class Astar:
         #g is set to zero
         self.searchstate.g = 0
         #the searchnodes heuristic is calculated
-        self.searchstate.calculateHeuristic()
+        self.searchstate.updatef
         #separate methods used to append to openlist.
         self.appendtoopen(self.searchstate,self.type)
         #Extra structure to see make it faster to check if node in openlist
         self.openset.add(self.searchstate)
         #While we have not arrived at the goal
-        while not self.searchstate.h ==0 :
-            #if openlist is empty, no solution is found, return false
-            if len(self.openlist) ==0 :
-                return False
-            #pop element with highest F from openlist
-            self.searchstate = self.popfromopen()
-            #remove from support structure
-            self.openset.remove(self.searchstate)
-            #add to closedset, this node is now about to be expanded
-            self.closedset.add(self.searchstate)
-            #Generate children, these children now get searchstate as parent
-            self.searchstate.children = self.searchstate.calculateNeighbours
-            #for each child
-            for succ in self.searchstate.children:
-                #if child in openset, it has already been created
-                if succ in self.openset:
-                    succ = self.popfromopen(self.type)
-                    self.openset.remove(succ)
 
-                if succ in self.closedset:
-                    x = self.closedset.pop(succ)
+    #Have to implement this method because gui is main loop
+    def do_one_step(self):
+        #if openlist is empty, no solution is found, return false
+        if len(self.openlist) ==0 :
+            return False
+        #pop element with highest F from openlist
+        self.searchstate = self.popfromopen(self.type)
+        #remove from support structure
+        self.openset.remove(self.searchstate)
+        #add to closedlist this node is now about to be expanded
+        self.closedlist.append(self.searchstate)
+        #add to support structure
+        self.closedset.add(self.searchstate)
+        #Generate children, these children now get searchstate as parent
+        self.searchstate.children = self.searchstate.calculateNeighbours()
+        #for each child
+        for succ in self.searchstate.children:
+            #if child in openset, it has already been created
+            if succ in self.openset:
+                succ = self.popfromopen(self.type)
+                self.openset.remove(succ)
+                if self.searchstate.g < succ.parent.g:
+                    succ.parent = self
+                    succ.calculateHeuristic()
+            if succ in self.closedset:
+                self.closedlist.remove(succ)
+                if self.searchstate.g < succ.parent.g:
+                    succ.propagatepathimprovements(self)
+                self.closedset.add(succ)
+            else:
+                self.appendtoopen(succ,self.type)
+                self.openset.add(succ)
+        if self.searchstate.h ==0:
+            return True
 
 
 
@@ -54,9 +69,18 @@ class Astar:
 
 
 
-    def propagatepathimprovements(self,parent):
-        return
+    def attach_and_eval(self,child,parent):
+        child.parent = parent
+        child.g = parent.g +1
+        child.updatef()
 
+    def propagate_path_improvement(self, parent):
+        for child in parent.children:
+            if parent.g +1  < child.g:
+                child.parent = parent
+                child.g = parent.g+1
+                child.updatef()
+                self.propagate_path_improvement(child)
 
     #method to pop from openlist type decides datastructure
     def popfromopen(self,type):
@@ -65,4 +89,8 @@ class Astar:
     #method to append to openlist type decides datastructure
     def appendtoopen(self,state,type):
         if self.type =="astar":
-            heappush(self.open,(state.f,state))
+            heappush(self.openlist,state)
+
+astar = Astar()
+while True:
+    astar.do_one_step()
--- state.py
+++ state.py
@@ -3,52 +3,51 @@ from heapq import heappop,heappush
 class State():
     def __init__(self,xpos,ypos,board,parent):
         #Initialises the searchstate. It has to know about board to calculate neighbours
+        self.board = board
         self.dimensions = board.dimensions
         self.xpos = xpos
         self.ypos = ypos
         self.h = float('inf')
-        self.parents = []
+        self.parent = parent
         self.g = float('inf')
-        self.calculateHeuristic(board)
+        self.calculateHeuristic()
         self.f = self.g + self.h
         self.children =[]
 
     def __repr__(self):
-        return self.type
+        return str(self.xpos)+" "+str(self.ypos)
     ##adds the new parent to the heap of parents. If it is at the top, it has the lowest g value
     def reparent(self, parent):
-        heappush(self.parents,(parent.g,parent))
-        self.g = self.parents[0][0]+1
-        self.calculateHeuristic()
-
+        self.parent = parent
+        self.updatef()
     #Calculates heuristic using manhattan distance
 
     def calculateHeuristic(self):
-        board = self.board
-        self.h = abs(self.xpos - board.goal[0])+ abs (self.ypos - board.goal[1])
-        self.updatef()
+        self.h = abs(self.xpos - int(self.board.goal[0]))+ abs (self.ypos - int(self.board.goal[1]))
     #updates f
     def updatef(self):
+        self.calculateHeuristic()
         self.f = self.h + self.g
     #Returns list of neighbours. Checks all directions. If they are within the grid and not a wall, a new
     #search state is created with self as parent.
-    def calculateNeighbours(self,board):
+    def calculateNeighbours(self):
+        board = self.board
         neighbours = []
-        if self.xpos-1  >=0 and self.ypos+1 < board.dimensions[1] and not board[self.xpos-1][self.ypos+1]=='#':
-            neighbours.append(State(self.xpos-1,self.ypos+1),board,self)
-        if self.xpos +1 < board.dimensions [0] and self.ypos +1 < board.dimensions[1] and not board[self.xpos+1][self.ypos+1]=='#':
+        if self.xpos-1  >=0 and self.ypos+1 < board.dimensions[1] and not board.grid[self.xpos-1][self.ypos+1]=='#':
+            neighbours.append(State(self.xpos-1,self.ypos+1,board,self))
+        if self.xpos +1 < board.dimensions[0] and self.ypos +1 < board.dimensions[1] and not board.grid[self.xpos+1][self.ypos+1]=='#':
             neighbours.append(State(self.xpos+1,self.ypos+1, board,self))
-        if self.xpos +1 < board.dimensions[0] and self.ypos -1 >=0 and not board[self.xpos+1][self.ypos-1]=='#':
+        if self.xpos +1 < board.dimensions[0] and self.ypos -1 >=0 and not  board.grid[self.xpos+1][self.ypos-1]=='#':
             neighbours.append(State(self.xpos+1,self.ypos-1, board,self))
-        if self.xpos -1>= 0 and self.ypos-1 >= 0 and not board[self.xpos-1][self.ypos-1] == '#':
+        if self.xpos -1>= 0 and self.ypos-1 >= 0 and not  board.grid[self.xpos-1][self.ypos-1] == '#':
             neighbours.append(State(self.xpos-1,self.ypos-1, board,self))
-        if self.xpos -1 >= 0 and not board[self.xpos-1][self.ypos] == '#':
+        if self.xpos -1 >= 0 and not board.grid[self.xpos-1][self.ypos] == '#':
             neighbours.append(State(self.xpos-1,self.ypos, board,self))
-        if self.xpos +1 < board.dimensions[0] and not board[self.xpos+1][self.ypos] == '#':
+        if self.xpos +1 < board.dimensions[0] and not board.grid[self.xpos+1][self.ypos] == '#':
             neighbours.append(State(self.xpos+1,self.ypos,board,self))
-        if self.ypos -1 >= 0 and not board[self.xpos][self.ypos-1] =='#':
+        if self.ypos -1 >= 0 and not board.grid[self.xpos][self.ypos-1] =='#':
             neighbours.append(State(self.xpos,self.ypos-1,board,self))
-        if self.ypos +1 < board.dimensions[1] and not board[self.xpos][self.ypos+1] =='#':
+        if self.ypos +1 < board.dimensions[1] and not board.grid[self.xpos][self.ypos+1] =='#':
             neighbours.append(State(self.xpos,self.ypos+1,board,self))
         return neighbours
     #Overrided to use in comparisons
@@ -60,7 +59,7 @@ class State():
     #Overrided to use in comparisons
     def __lt__(self, other):
         if self == other:
-            return self.h < other
+            return self.h < other.h
         return self.f < other.f
     #Overrided to use in comparisons
     def __gt__(self, other):
