f530f2bab926b42fdb79d94040698f0061f2503f
Paul Philip Mitchell <paulpmit@gmail.com>
2015-09-22 21:03:53 +0200

    Added skeleton for csp

---
+++ Modul2/csp.py
@@ -0,0 +1,80 @@
+__author__ = 'paulpm'
+
+""" NOTE:
+    Classes Constraint, Vertex and CSP should go into constraint.py, vertex.py and csp.py respectively.
+    They are all in this class only during the initial stages of development.
+"""
+
+
+class Constraint:
+
+    def __init__(self, vertices):
+        self.vertices = vertices
+
+
+class Vertex:
+
+    def __init__(self, index, x, y):
+        self.index = index
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'n' + str(self.index)
+
+
+class CSP:
+
+    def __init__(self):
+        self.variables = []
+        self.domains = {}
+        self.constraints = {}
+        self.queue = []
+
+    def makefunc(self, var_names, expression, environment=globals()):
+        args = ""
+        for n in var_names:
+            args = args + ',' + n
+        return eval('(lambda ' + args[1:] + ': ' + expression + ')', environment)
+
+    def revise(self):
+        raise NotImplementedError
+
+    def domain_filter(self):
+        raise NotImplementedError
+
+    def rerun(self):
+        raise NotImplementedError
+
+    def initialize_queue(self):
+        raise NotImplementedError
+
+
+def create_csp(graph_file):
+    csp = CSP()
+    f = open(graph_file, 'r')
+    number_of_vertices, number_of_edges = [int(x) for x in f.readline().strip().split(' ')]
+
+    for i in range(number_of_vertices):
+        index, x, y = [i for i in f.readline().strip().split(' ')]
+        vertex = Vertex(int(index), float(x), float(y))
+        csp.variables.append(vertex)
+        csp.constraints[vertex] = []
+
+    for j in range(number_of_edges):
+        i1, i2 = [int(i) for i in f.readline().strip().split(' ')]
+        this_vertex = csp.variables[i1]
+        other_vertex = csp.variables[i2]
+        csp.constraints[this_vertex].append(Constraint([this_vertex, other_vertex]))
+        csp.constraints[other_vertex].append(Constraint([other_vertex, this_vertex]))
+
+    f.close()
+
+
+def main():
+    create_csp("graph-color-1.txt")
+
+if __name__ == "__main__":
+    main()
+
+
