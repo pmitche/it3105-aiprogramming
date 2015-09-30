ab54269d898609ccf381bddbcfa891ada72d0e5f
sondrehd <sondredyvik@gmail.com>
2015-09-30 19:24:57 +0200

    everything works. ish

--- Modul2/csp_gui.py
+++ Modul2/csp_gui.py
@@ -7,24 +7,69 @@ import variable as cspvariable
 import constraint as cspconstraint
 import astarmod2
 import time
+import gc
 
 class csp_gui:
     def __init__(self, parent):
-        self.parent = Frame(parent, width=500, height=500)
+        self.width = 800
+        self.height = 800
+        self.parent = Frame(parent, width =self.width, height =self.width)
         self.vertex_dict = {}
         self.edge_dict = {}
         self.colors = [0, 1, 2, 3, 4, 5]
         self.number_to_color ={0: 'black', 1: "yellow", 2: "pink", 3: "purple", 4: "red", 5: "blue"}
-        self.csp = self.create_csp("spiral-500-4-color1.txt", 4)
-        self.canvas = Canvas(master=self.parent,width=500, height=500)
+        self.csp = None
+        self.canvas = Canvas(master=self.parent,width= self.width, height =self.height)
         self.canvas.pack()
         self.parent.pack()
-        self.drawmap()
-        self.astar = astarmod2.Astarmod2(self.csp)
-        self.csp.initialize_queue(self.astar.searchstate)
-        self.csp.domain_filter()
-        self.time = time.time()
-        self.run_astar()
+        self.board = None
+        self.running = False
+
+
+        menubar = Menu(parent)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Board 1", command=lambda: self.setboard("graph-color-1.txt"))
+        filemenu.add_command(label="Board 2", command=lambda: self.setboard("graph-color-2.txt"))
+        filemenu.add_command(label="Board 3", command=lambda: self.setboard("rand-50-4-color1.txt"))
+        filemenu.add_command(label="Board 4", command=lambda: self.setboard("rand-100-4-color1.txt"))
+        filemenu.add_command(label="Board 5", command=lambda: self.setboard("rand-100-6-color1.txt"))
+        filemenu.add_command(label="Board 6", command=lambda: self.setboard("spiral-500-4-color1.txt"))
+        filemenu.add_command(label="Custom board", command=lambda: self.openfile())
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=parent.quit)
+        menubar.add_cascade(label="Board", menu=filemenu)
+
+        editmenu = Menu(menubar, tearoff=0)
+        editmenu.add_command(label="K=1", command=lambda: self.run(self.board, 1))
+        editmenu.add_command(label="K=2", command=lambda: self.run(self.board, 2))
+        editmenu.add_command(label="K=3", command=lambda: self.run(self.board, 3))
+        editmenu.add_command(label="K=4", command=lambda: self.run(self.board, 4))
+        editmenu.add_command(label="K=5", command=lambda: self.run(self.board, 5))
+        editmenu.add_command(label="K=6", command=lambda: self.run(self.board, 6))
+
+        menubar.add_cascade(label="Colors", menu=editmenu)
+
+        parent.config(menu=menubar)
+
+    def openfile(self):
+        self.setboard(askopenfilename(parent=self.parent))
+
+    def setboard(self,filename):
+        self.board = filename
+
+    def run(self,filename,k):
+        if self.running is False:
+            if self.board is None:
+                self.setboard("graph-color-1")
+            self.canvas.delete("all")
+            self.running = True
+            self.csp = self.create_csp(self.board, k)
+            self.drawmap()
+            self.astar = astarmod2.Astarmod2(self.csp)
+            self.csp.initialize_queue(self.astar.searchstate)
+            self.csp.domain_filter()
+            self.time = time.time()
+            self.run_astar()
 
     def normalize_coordinates(self,xpos,ypos):
         highestx = max([float(var.x) for var in self.csp.variables])
@@ -32,7 +77,7 @@ class csp_gui:
         highesty = max([float(var.y) for var in self.csp.variables])
         lowesty = min([float(var.y )for var in self.csp.variables])
         old_range =float(highestx-lowestx)
-        new_range = float(480-20)
+        new_range = float(self.width-20)
         new_x = (((float(xpos)-lowestx)*new_range)/old_range) + 20
         old_range = float(highesty-lowesty)
         new_y = (((float(ypos)-lowesty)*new_range)/old_range) + 20
@@ -45,16 +90,21 @@ class csp_gui:
             if len(self.astar.searchstate.domains[key]) == 1:
                 self.canvas.itemconfig(self.vertex_dict[key.index], fill=self.number_to_color[self.astar.searchstate.domains[key][0]])
         if len(self.astar.openlist)> 0:
-            self.parent.after(50, lambda: self.run_astar())
+            self.parent.after(1, lambda: self.run_astar())
         else:
             for key in self.astar.searchstate.domains.keys():
-                self.canvas.itemconfig(self.vertex_dict[key.index], fill=self.number_to_color[self.astar.searchstate.domains[key][0]])
+                if len(self.astar.searchstate.domains[key]) == 1:
+                    self.canvas.itemconfig(self.vertex_dict[key.index], fill=self.number_to_color[self.astar.searchstate.domains[key][0]])
+                else:
+                    self.canvas.itemconfig(self.vertex_dict[key.index], fill="white")
             for key in self.csp.constraints.keys():
                 for constraint in self.csp.constraints[key]:
                     if self.astar.searchstate.domains[constraint.vertices[0]] ==self.astar.searchstate.domains[constraint.vertices[1]]:
-                        print "suckadickanddie",  self.astar.searchstate.domains[constraint.vertices[0]], self.astar.searchstate.domains[constraint.vertices[1]]
+                        Canvas.create_text(self.canvas,400,400,fill="red",text="NO SOLUTION FOUND")
             print "DONE"
             print time.time() - self.time
+            self.running = False
+            gc.collect()
 
 
 
